52af9674689d5c3485f9d0a41c921d5a67d155c8
Egor <vev_113@mail.ru>
2024-01-26 10:14:58 +0300

    Замена заглавных букв на строчные в строке

---
+++ Desktop/ДЗ. Массивы и строки/ДЗ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Исходная строка с латинскими буквами в обоих регистрах
+        string originalString = "HeLLoWorLD";
+
+        // Преобразование всех заглавных букв в строчные
+        string resultString = originalString.ToLower();
+
+        // Вывод результата
+        Console.WriteLine("Исходная строка: " + originalString);
+        Console.WriteLine("Результат: " + resultString);
+    }
+}
