c9deb0d51c6be63bf0c7616fc1986b46afbe83ba
Egor <vev_113@mail.ru>
2024-01-26 10:18:23 +0300

    Проверка строки на палиндром

---
+++ Desktop/ДЗ. Массивы и строки/ДЗ 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("Это палиндром!");
+        }
+        else
+        {
+            Console.WriteLine("Это не палиндром.");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        // Удаляем пробелы и делаем все буквы строчными
+        string cleanedStr = str.Replace(" ", "").ToLower();
+
+        // Создаем массив символов из строки
+        char[] charArray = cleanedStr.ToCharArray();
+
+        // Переворачиваем массив символов
+        Array.Reverse(charArray);
+
+        // Сравниваем перевернутую строку с оригинальной
+        string reversedStr = new string(charArray);
+
+        return cleanedStr == reversedStr;
+    }
+}
